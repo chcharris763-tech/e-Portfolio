8a1cbb5bd849f6d8c5809717dfab33dc360d4d10
chcharris763-tech <chcharris763@gmail.com>
2025-09-19 10:08:45 -0500

    help with dark mode

--- app.js
+++ app.js
@@ -32,10 +32,10 @@ function contact(event) {
   loading.classList += " modal__overlay--visible";
   emailjs
     .sendForm(
-      "service_80ih0if",
-      "template_d9refyl",
+      "service_zla80sz",
+      "template_0g676z5",
       event.target,
-      "user_K1PoFs8pB2YVWStDxrUls"
+      "dNCD6NS2HWkNupB87"
     )
     .then(() => {
       loading.classList.remove("modal__overlay--visible");
@@ -44,7 +44,7 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me directly on email@email.com"
+        "The email service is temporarily unavailable. Please contact me directly on chcharris763@gmail.com"
       );
     });
 }
